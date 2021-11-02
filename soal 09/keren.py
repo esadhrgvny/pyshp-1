77c3a850e9f65cd8080927996077deb6579087f8
esadhrgvny <61672278+esadhrgvny@users.noreply.github.com>
2021-11-02 21:16:22 +0700

    Add files via upload

--- soal 09/keren.py
+++ soal 09/keren.py
@@ -1,4 +1,3 @@
-
 import shapefile
 w=shapefile.Writer()
 w.shapeType
@@ -9,10 +8,7 @@ w.field("kolom2","C")
 w.record("ngek","satu")
 w.record("crot","dua")
 
-
-
 w.poly(parts=[[[1,3],[5,3], [5,2],[1,2], [1,3]]],shapeType=shapefile.POLYLINE)
 w.poly(parts=[[[1,6],[5,6], [5,9],[1,9], [1,6]]],shapeType=shapefile.POLYLINE)
-
 
 w.save("soal9")
